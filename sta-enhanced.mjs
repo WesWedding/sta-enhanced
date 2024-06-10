64e9de86fd4dd84d9c68cee77cb21a9e39e9b060
Weston Wedding <wes@wawco.com>
2024-06-09 17:00:59 -0700

    WIP of re-org, introducing some ugly style issues.
    Some preliminary groundwork for a tab system added, but a lot of work left to do.  Especially with styling.

---
+++ sta-enhanced.mjs
@@ -0,0 +1,51 @@
+import { STACharacterSheet } from '../../systems/sta/module/actors/sheets/character-sheet.js'
+
+Hooks.once("init", () => {
+  
+  // Register sheets.
+  Actors.registerSheet("sta-enhanced", STACharacterEnhancedSheet, {
+    types: ["character"],
+    label: "ENHANCED CHAR SHEET", // TODO: Localize.
+    // makeDefault: true
+  });
+  
+  // Preload Handlebars partials.
+  preloadHandlebarsTemplates();
+});
+
+class STACharacterEnhancedSheet extends STACharacterSheet {
+  /** @inheritDoc */
+  static get defaultOptions() {
+    return foundry.utils.mergeObject(super.defaultOptions, {
+      classes: super.defaultOptions.classes.concat(["sta-enhanced"]),
+      tabs: [
+        {navSelector: "nav", contentSelector: ".tab-area", initial: "character"}
+      ],
+    }, {insertValues: true});
+  }
+  
+  /** @inheritDoc */
+  getData(options = {}) {
+    const context = super.getData(options);
+    
+    return context;
+  }
+  
+  /** @inheritDoc */
+  get template() {
+    if ( !game.user.isGM && this.actor.limited ) return "systems/sta/templates/actors/limited-sheet.html";
+    return "modules/sta-enhanced/templates/actors/character-sheet.hbs";
+  }
+}
+
+
+async function preloadHandlebarsTemplates() {
+  const paths = {
+    [`sta-enhanced.tabs.character`]: "modules/sta-enhanced/templates/actors/tabs/character.hbs",
+    [`sta-enhanced.tabs.belongings`]: "modules/sta-enhanced/templates/actors/tabs/belongings.hbs",
+    [`sta-enhanced.tabs.reputation`]: "modules/sta-enhanced/templates/actors/tabs/reputation.hbs",
+    [`sta-enhanced.tabs.biography`]: "modules/sta-enhanced/templates/actors/tabs/biography.hbs",
+  };
+  
+  return loadTemplates(paths);
+}
