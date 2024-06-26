8626802ab79dbc093af6e28c849a893fc93ccd85
Weston Wedding <wes@wawco.com>
2024-06-26 13:27:27 -0700

    WIP: Porting portions of PF2E's migration architecture over.
    I want to rename some flag names, so I'm exploring making use of the really neat looking (albeit complex) stuff they're doing to migrate data.

--- sta-enhanced.mjs
+++ sta-enhanced.mjs
@@ -1,7 +1,57 @@
 import { STACharacterSheet } from '../../systems/sta/module/actors/sheets/character-sheet.js'
 import { STAActor } from '../../systems/sta/module/actors/actor.js'
+import { MigrationRunner } from './migrations/MigrationRunner.mjs'
+
+/**
+ * Store the world system and schema versions for the first time.
+ *
+ * Many thanks to the PF2E system for this implementation.
+ * @link https://github.com/foundryvtt/pf2e/blob/1da6f466291767c1e72ef26d34052c2c64943872/src/scripts/store-versions.ts#L3
+ */
+async function storeInitialWorldVersions() {
+  if (!game.user.hasRole(CONST.USER_ROLES.GAMEMASTER)) return;
+
+  const storedModuleVersion = game.settings.storage.get("world").getItem("sta-enhanced.worldModuleVersion");
+  console.log("stored module version");
+  if (!storedModuleVersion) {
+    // await game.settings.set("sta-advanced", "worldModuleVersion", game.modules["sta-enhanced"].version);
+  }
+
+  const storedSchemaVersion = game.settings.storage.get("world").getItem("sta-enhanced.worldSchemaVersion");
+  console.log("stored schema version", storedSchemaVersion);
+  if (!storedSchemaVersion) {
+    console.log("no schema found, setting default");
+    const minVersion = MigrationRunner.RECOMMENDED_SAFE_VERSION;
+    const currentVersion = game.actors.size === 0
+        ? game.settings.get("sta-enhanced", "worldSchemaVersion")
+        : Math.max(
+            Math.min(...new Set(game.actors.map((actor) => {
+              console.log("actor", actor.flags["sta-enhanced"]?.schemaVersion);
+              return actor.flags["sta-enhanced"]?.schemaVersion ?? minVersion
+            }))),
+            minVersion
+        );
+
+    console.log("todo: set the schema to", currentVersion);
+    await game.settings.set("sta-enhanced", "worldSchemaVersion", currentVersion);
+  }
+}
 
 Hooks.once("init", () => {
+
+  console.log(game.modules)
+
+  // Register settings
+  game.settings.register("sta-enhanced", "worldSchemaVersion", {
+    name: "sta-enhanced.settings.worldSchemaVersion.Name",
+    hint: "sta-enhanced.settings.worldSchemaVersion.Hint",
+    scope: "world",
+    config: true,
+    default: MigrationRunner.LATEST_SCHEMA_VERSION,
+    type: Number,
+    requiresReload: true,
+  });
+
 
   // Register sheets.
   Actors.registerSheet("sta-enhanced", STACharacterEnhancedSheet, {
@@ -9,10 +59,54 @@ Hooks.once("init", () => {
     label: "sta-enhanced.SheetClassCharacter",
     // makeDefault: true
   });
+
+  console.log("Has type data????", STAActor.hasTypeData);
+
+  console.log("CONFIG.Actor.dataModels", CONFIG.Actor.dataModels);
+
+
   
   // Preload Handlebars partials.
   preloadHandlebarsTemplates();
 });
+
+Hooks.once("ready", () => {
+  console.log("ready");
+
+
+  // Determine whether a system migration is required and feasible.
+  const currentVersion = game.settings.get("sta-enhanced", "worldSchemaVersion");
+  console.log("worldSchema version detected at ready", currentVersion);
+
+  // Run flag data migrations.
+  storeInitialWorldVersions().then(async () => {
+    // Ensure only a single GM will run migrations if multiple users are logged in.
+    if (game.user !== game.users.activeGM) return;
+
+    const migrationRunner = new MigrationRunner();
+    if (migrationRunner.needsMigration()) {
+      if (currentVersion && currentVersion < MigrationRunner.MINIMUM_SAFE_VERSION) {
+        ui.notifications.error(
+          `Your STA Enhanced data is from too old a Foundry version and cannot be reliably migrated to the latest version.  An attempt will be made, but errors may occur.`,
+            {permanent: true},
+        );
+      }
+      await migrationRunner.runMigration();
+    }
+  });
+});
+
+Hooks.on('createActor', (actor, options, userId) => {
+  if (actor.type !== 'character') return;
+  console.warn('STA Character created!', actor, options, userId);
+});
+
+Hooks.on('updateActor', (actor, change, options, userId) => {
+  if (actor.type !== 'character') return;
+
+
+  console.warn('STA character updated!', actor, change, options, userId);
+})
 
 class STACharacterEnhancedSheet extends STACharacterSheet {
   /** @inheritDoc */
