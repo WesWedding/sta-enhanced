191cbba4c12109429ea7e04a88c26e5a3066572d
Weston Wedding <wes@wawco.com>
2024-08-23 10:29:04 -0700

    Updated #21: Positive/Negative reputation fields have label settings.
    STA introduced the new rep system as a Klingon feature, so it seems to make sense to support Klingon terminology.

--- settings.mjs
+++ settings.mjs
@@ -1,7 +1,13 @@
 export const CONSTS = Object.freeze({
-  reputationVariant: {
-    'original': 'original',
-    '1stEdNew': '1stEdNew',
+  reputation: {
+    variant: {
+      'original': 'original',
+      '1stEdNew': '1stEdNew',
+    },
+    resultLabels: {
+      generic: 'generic',
+      klingon: 'klingon',
+    },
   },
 });
 
@@ -30,16 +36,28 @@ export function registerModuleSettings() {
 
   // The Reputation version to be used in Character sheets.
   game.settings.register('sta-enhanced', 'reputationVariant', {
-    name: 'sta-enhanced.settings.reputationVariant.Name',
-    hint: 'sta-enhanced.settings.reputationVariant.Hint',
+    name: 'sta-enhanced.settings.reputation.variant.Name',
+    hint: 'sta-enhanced.settings.reputation.variant.Hint',
     scope: 'world',
     config: true,
-    default: CONSTS.reputationVariant['1stEdNew'],
+    default: CONSTS.reputation.variant['1stEdNew'],
     choices: {
-      [CONSTS.reputationVariant.original]: 'sta-enhanced.settings.reputationVariant.variant.original',
-      [CONSTS.reputationVariant['1stEdNew']]: 'sta-enhanced.settings.reputationVariant.variant.1stEdNew',
+      [CONSTS.reputation.variant.original]: 'sta-enhanced.settings.reputation.variant.original',
+      [CONSTS.reputation.variant['1stEdNew']]: 'sta-enhanced.settings.reputation.variant.1stEdNew',
     },
     onChange: _resetMaxSystemReputation,
+  });
+
+  game.settings.register('sta-enhanced', 'reputationLabels', {
+    name: 'sta-enhanced.settings.reputation.reputationLabels.Name',
+    hint: 'sta-enhanced.settings.reputation.reputationLabels.Hint',
+    score: 'world',
+    config: true,
+    default: CONSTS.reputation.resultLabels.generic,
+    choices: {
+      [CONSTS.reputation.resultLabels.generic]: 'sta-enhanced.settings.reputation.labels.generic',
+      [CONSTS.reputation.resultLabels.klingon]: 'sta-enhanced.settings.reputation.labels.klingon',
+    },
   });
 }
 
@@ -51,12 +69,12 @@ export function registerModuleSettings() {
  */
 function _resetMaxSystemReputation(variant) {
   // Original variant max rep was 20.
-  if (variant === CONSTS.reputationVariant.original) {
+  if (variant === CONSTS.reputation.variant.original) {
     game.settings.set('sta', 'maxNumberOfReputation', 20);
   }
 
   // Klingon max rep is 5.
-  if (variant === CONSTS.reputationVariant['1stEdNew']) {
+  if (variant === CONSTS.reputation.variant['1stEdNew']) {
     game.settings.set('sta', 'maxNumberOfReputation', 5);
   }
 }
