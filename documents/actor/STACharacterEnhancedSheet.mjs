75c5ce6de9551d45ae1d920e9d1283564efce631
Weston Wedding <wes@wawco.com>
2024-09-26 16:42:50 -0700

    Removed: Custom chat card implementation (#24).
    Left behind click handlers for reroll buttons, which now routs the request to the System's prompts and workflow.

--- documents/actor/STACharacterEnhancedSheet.mjs
+++ documents/actor/STACharacterEnhancedSheet.mjs
@@ -1,7 +1,4 @@
 import { STACharacterSheet } from '../../../../systems/sta/module/actors/sheets/character-sheet.js';
-import { ItemChatCard } from '../../chat/ItemChatCard.mjs';
-import { ItemHelpers } from '../../helpers/ItemHelpers.mjs';
-import { RollHelpers } from '../../helpers/RollHelpers.mjs';
 import { CONSTS as SETTINGS_CONSTS } from '../../settings.mjs';
 
 // After changes in the System, this global is required to keep sheets working.  Not ideal!
@@ -74,7 +71,6 @@ export class STACharacterEnhancedSheet extends STACharacterSheet {
     if (!game.user.isGM && this.actor.limited) return;
     this._handleStressMod($html);
     this._handleTooltipClicks($html);
-    this._replaceSystemItemRolls($html);
   }
 
   _handleStressMod($html) {
@@ -120,40 +116,6 @@ export class STACharacterEnhancedSheet extends STACharacterSheet {
         $('.weapon-tooltip-container').addClass('hide').removeAttr('style');
         $('#weapon-tooltip-container-' + weaponId).removeClass('hide').height($('#weapon-tooltip-text-' + weaponId)[0].scrollHeight + 5);
       }
-    });
-  }
-
-  /**
-   * Swap in new chat card icons.
-   *
-   * Hide the existing images that the system provides with new ones that have
-   * their own handlers attached.  The existing system ones are not extensible
-   * so they are being replaced entirely.
-   *
-   * @param {jQuery} $html
-   * @private
-   */
-  _replaceSystemItemRolls($html) {
-    const rollIcons = $html.find('.chat, .rollable');
-    const clones = rollIcons.clone();
-    rollIcons.each((idx, element) => {
-      $(element).after(clones[idx]);
-    });
-    rollIcons.hide();
-
-    clones.on('click', async (event) => {
-      const itemType = $(event.currentTarget).parents('.entry')[0].getAttribute('data-item-type');
-      const itemId = $(event.currentTarget).parents('.entry')[0].getAttribute('data-item-id');
-      const item = this.actor.items.get(itemId);
-
-      const numDice = ItemHelpers.getNumDamageDiceFor(item);
-      let damageRoll = null;
-      if (itemType === 'characterweapon' || itemType === 'starshipweapon') {
-        damageRoll = await RollHelpers.performChallengeRoll(numDice);
-      }
-
-      const card = new ItemChatCard(item, damageRoll);
-      await card.sendToChat(this.actor);
     });
   }
 }
